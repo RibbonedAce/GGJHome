f6644441c3adc07b7100e1df26110a62a19ea746
henry-ftw1 <44074122+henry-ftw1@users.noreply.github.com>
2019-01-26 11:06:11 -0800

    t

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    //[SerializeField] public AmmoType m_ammo;
+    public GameObject projectile;
+    //public float speed = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Shoot in the forward direction of this object
+        Vector3 fwdDirection = this.transform.forward;
+
+        Instantiate(projectile, this.transform.position, Quaternion.Euler(fwdDirection));
+
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+    [SerializeField]
+    public float m_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = this.GetComponent<Rigidbody>();
+        m_rigidbody.AddForce(transform.forward * m_speed, ForceMode.VelocityChange);
+        Destroy(gameObject, 7f);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
